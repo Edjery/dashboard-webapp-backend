a4dc25d42bfcf7115cb13c933606063192ce31f0
EJ <edisgumbao@gmail.com>
2024-03-29 12:12:18 +0800

    Authenticated user Register in admin

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from api.models import DashboardUser
+from api.models import AuthenticatedUser, DashboardUser
 
 @admin.register(DashboardUser)
 class DashboardUserAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'email', 'password')
+
+@admin.register(AuthenticatedUser)
+class AuthenticatedUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status', 'user')
